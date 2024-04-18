11180271177d6719f23f6a9e8287cf835d04c7c0
Lily Whitford <lilywhitford2@gmail.com>
2024-04-18 21:52:40 +0000

    change left join to inner join

--- 1_queries/2_property_listings_by_city.sql
+++ 1_queries/2_property_listings_by_city.sql
@@ -1,6 +1,6 @@
 SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
 FROM properties
-LEFT JOIN property_reviews ON property_id = properties.id
+JOIN property_reviews ON  properties.id = property_id
 WHERE city LIKE '%ancouv%'
 GROUP BY properties.id 
 HAVING avg(property_reviews.rating) >= 4
